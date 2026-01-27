dda11e9f6466082cec4abf7c7f6875b460a128a9
Desmond Jung <jungdesmond@gmail.com>
2026-01-27 14:09:57 -0800

    Successful authentication to tradovate api

--- app/ingestion/tradovate.py
+++ app/ingestion/tradovate.py
@@ -1,21 +1,43 @@
 import requests
+import uuid
 
-url = "https://demo.tradovateapi.com/v1/auth/accesstokenrequest"
-headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
+access_token = None
+
+def authenticate():
+    global access_token
+
+    username = "Google:115790771135467284232"
+    password = "Djm0nd!23"  
+
+    url = "https://demo.tradovateapi.com/v1/auth/accesstokenrequest"
+
+    body = {
+    "name": username,
+    "password": password,
+    "appId": "tradovate",  # Changed from "TradingJournal"
+    "appVersion": "0.0.1",  # Changed from "1.0"
+    "deviceId": str(uuid.uuid4()),
+    "cid": "9574",  # Changed to string
+    "sec": "94bbf03e-a583-4df7-b96e-78df5500f5b8"
     }
-body = {
-    "name": "APEX_419310",
-    "password": "AF5@0$F@2c##",
-    "appId": "TestApp",
-    "appVersion": "1.0",
-    "cid": 8,
-    "deviceId": "test-device-123",
-    "sec": "test-sec-456"   
-}
 
-response = requests.post(url, headers=headers, json=body)
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
 
-print("Status Code:", response.status_code)
-print("Response:", response.json())
+    print("Authenticating...")
+    response = requests.post(url, json=body, headers = headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        access_token = data.get('accessToken')
+        print("Authenticated! Token saved")
+        return True
+    else:
+        print(f"Failed{response.json()}")
+        return False
+
+if __name__ == '__main__':
+    authenticate()
+    print("Token stored")
